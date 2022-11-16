61f0e47843a5c19f658b9dac4f501443275e866e
tuphan-dn <tuphan@descartes.network>
2022-11-16 15:05:09 +0700

    ionicon

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,3 +1,4 @@
+import IonIcon from '@sentre/antd-ionicon'
 import { Image, Col, Layout, Row, Space, Typography } from 'antd'
 
 import logo from 'static/images/solanaLogo.svg'
@@ -8,9 +9,18 @@ function View() {
     <Layout className="container">
       <Row gutter={[24, 24]}>
         <Col span={24} style={{ textAlign: 'center' }}>
-          <Space direction="vertical">
+          <Space direction="vertical" size={24}>
             <Image src={logo} preview={false} width={256} />
             <Typography.Title level={1}>React + Solana = DApp</Typography.Title>
+            <Typography.Text type="secondary">
+              <Space>
+                <IonIcon name="logo-react" />
+                +
+                <IonIcon name="logo-solana" />
+                =
+                <IonIcon name="rocket" />
+              </Space>
+            </Typography.Text>
           </Space>
         </Col>
       </Row>
