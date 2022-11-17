e7fdbae17693e0f9d64501b361c259930ff09400
tuphan-dn <tuphan@descartes.network>
2022-11-17 11:34:55 +0700

    lecture 2

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,37 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import View from 'view'
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react'
+import {
+  Coin98WalletAdapter,
+  PhantomWalletAdapter,
+} from '@solana/wallet-adapter-wallets'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 
+import configs from 'configs'
 import reportWebVitals from 'reportWebVitals'
 
 import 'static/styles/index.less'
 
+const {
+  rpc: { endpoint },
+} = configs
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <View />
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider
+        wallets={[new PhantomWalletAdapter(), new Coin98WalletAdapter()]}
+        autoConnect
+      >
+        <WalletModalProvider>
+          <View />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   </BrowserRouter>,
 )
 
--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,14 +1,97 @@
 import IonIcon from '@sentre/antd-ionicon'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { Keypair, SystemProgram, Transaction } from '@solana/web3.js'
 
 import { Image, Col, Layout, Row, Space, Typography, Button } from 'antd'
+import { useCallback, useEffect, useState } from 'react'
 
 import logo from 'static/images/solanaLogo.svg'
+import brand from 'static/images/solanaLogoMark.svg'
 import './index.less'
 
 function View() {
+  const { connection } = useConnection()
+  const { publicKey, sendTransaction } = useWallet()
+  const [balance, setBalance] = useState(0)
+  const [loading, setLoading] = useState(false)
+
+  const getMyBalance = useCallback(async () => {
+    if (!publicKey) return setBalance(0)
+    // Read on-chain balance
+    const lamports = await connection.getBalance(publicKey)
+    return setBalance(lamports)
+  }, [connection, publicKey])
+
+  const airdrop = useCallback(async () => {
+    try {
+      setLoading(true)
+      if (publicKey) {
+        // Request SOL airdrop
+        await connection.requestAirdrop(publicKey, 10 ** 8)
+        // Reload balance
+        return getMyBalance()
+      }
+    } catch (er: any) {
+      console.log(er.message)
+    } finally {
+      return setLoading(false)
+    }
+  }, [connection, publicKey, getMyBalance])
+
+  const transfer = useCallback(async () => {
+    try {
+      setLoading(true)
+      if (publicKey) {
+        // Create a "transfer" instruction
+        const instruction = SystemProgram.transfer({
+          fromPubkey: publicKey,
+          toPubkey: Keypair.generate().publicKey,
+          lamports: 10 ** 8,
+        })
+        // Create a transaction and add the instruction intot it
+        const transaction = new Transaction().add(instruction)
+        // Wrap on-chain info to the transaction
+        const {
+          context: { slot: minContextSlot },
+          value: { blockhash, lastValidBlockHeight },
+        } = await connection.getLatestBlockhashAndContext()
+        // Send and wait for the transaction confirmed
+        const signature = await sendTransaction(transaction, connection, {
+          minContextSlot,
+        })
+        await connection.confirmTransaction({
+          blockhash,
+          lastValidBlockHeight,
+          signature,
+        })
+        // Reload balance
+        return getMyBalance()
+      }
+    } catch (er: any) {
+      console.log(er.message)
+    } finally {
+      return setLoading(false)
+    }
+  }, [connection, publicKey, getMyBalance, sendTransaction])
+
+  useEffect(() => {
+    getMyBalance()
+  }, [getMyBalance])
+
   return (
     <Layout className="container">
       <Row gutter={[24, 24]}>
+        <Col span={24}>
+          <Row gutter={[24, 24]}>
+            <Col flex="auto">
+              <img alt="logo" src={brand} height={16} />
+            </Col>
+            <Col>
+              <WalletMultiButton />
+            </Col>
+          </Row>
+        </Col>
         <Col span={24} style={{ textAlign: 'center' }}>
           <Space direction="vertical" size={24}>
             <Image src={logo} preview={false} width={256} />
@@ -22,9 +105,27 @@ function View() {
                 <IonIcon name="rocket" />
               </Space>
             </Typography.Text>
-            <Button type="primary" size="large">
-              Let's go
-            </Button>
+            <Typography.Title>
+              My Balance: {balance / 10 ** 9} SOL
+            </Typography.Title>
+            <Space>
+              <Button
+                type="primary"
+                size="large"
+                onClick={airdrop}
+                loading={loading}
+              >
+                Airdrop
+              </Button>
+              <Button
+                type="primary"
+                size="large"
+                onClick={transfer}
+                loading={loading}
+              >
+                Transfer
+              </Button>
+            </Space>
           </Space>
         </Col>
       </Row>
